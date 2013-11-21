69fae287117cb954ea6a4899110cf0a5d6999d08
Nicki <nkeszler10@gmail.com>
2013-11-21 12:10:46 +0000

    Simple Sudoku Solved

--- lib/grid.rb
+++ lib/grid.rb
@@ -9,7 +9,7 @@ class Grid
 	end
 
 	def extract_row_as_cells(split_array)
-		split_array.slice!(0,9).map do |num|   
+		split_array.slice!(0,9).map do |num|
 			convert_num_to_cell(num)
 		end
 	end
@@ -35,6 +35,30 @@ class Grid
 	def assign_cell_positions_in(row, y_index)
 		row.each_with_index do |cell, x_index|
 			cell.position = {:y_index => y_index, :x_index => x_index, :box => determine_box(y_index, x_index)}
+		end
+	end
+
+	def show_neighbors_for(position_piece, position_piece_value)
+		neighbors = []
+		cells.each {|row| row.each {|num| neighbors << num.value if num.position[position_piece] == position_piece_value}}
+		neighbors
+	end
+
+	def show_neighbors(cell)
+		(show_neighbors_for(:x_index, cell.position[:x_index]) + show_neighbors_for(:y_index, cell.position[:y_index]) + show_neighbors_for(:box, cell.position[:box]) - ["0"]).uniq.sort
+	end
+
+	def solve_cell(cell)
+		(["1","2","3","4","5","6","7","8","9"] - show_neighbors(cell)).length == 1 ? cell.value = (["1","2","3","4","5","6","7","8","9"] - show_neighbors(cell))[0] : cell.value
+	end
+
+	def solved?
+		self.cells.each {|row| row.each {|cell| return false if !cell.solved?}}
+	end
+
+	def solve
+		while !solved?
+			self.cells.each {|row| row.each{|cell| solve_cell(cell) if !cell.solved?}}
 		end
 	end
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -11,6 +11,10 @@ describe Grid do
 		expect(grid.cells[0][0].class).to eq(Cell)
 	end
 
+	it "Should know if its solved" do
+		expect(grid.solved?).not_to be_true
+	end
+
 	it "Should know where each cell is located and its value" do
 		expect(grid.cells[2][5].value).to eq("8")
 		expect(grid.cells[2][5].position).to eq({:y_index => 2, :x_index => 5, :box => 1})
@@ -19,7 +23,23 @@ describe Grid do
 	end
 
 	it "Should be able to determine a cells neighbors" do
-		
+		cell = Cell.new
+		cell.position = {:y_index => 0, :x_index => 0, :box => 0}
+		expect(grid.show_neighbors(cell)).to eq(["1","3","4","5","2","7","8","9"].sort)
+	end
+
+	it "Should solve a cell" do
+		cell = Cell.new
+		cell.value = "0"
+		cell.position = {:y_index => 0, :x_index => 0, :box => 0}
+		grid.solve_cell(cell)
+		expect(cell.value).to eq("6")
+	end
+
+	it "Should solve the whole game" do
+		grid.solve
+		expect(grid.solved?).to be_true
+		expect(grid.cells.flatten.map{|cell| cell.value}.join).to eq('615493872348127956279568431496832517521746389783915264952681743864379125137254698')
 	end
 
 
